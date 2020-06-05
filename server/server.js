5b437a3b63face2e8b926fe0d7803e1b1c17f020
James Quick <james.q.quick@outlook.com>
2020-06-05 07:40:31 -0500

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const { cloudinary } = require('./utils/cloudinary');
+const express = require('express');
+const app = express();
+var cors = require('cors');
+
+app.use(express.static('public'));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors());
+
+app.get('/api/images', async (req, res) => {
+    const { resources } = await cloudinary.search
+        .expression('folder:dev_setups')
+        .sort_by('public_id', 'desc')
+        .max_results(30)
+        .execute();
+
+    const publicIds = resources.map((file) => file.public_id);
+    res.send(publicIds);
+});
+app.post('/api/upload', async (req, res) => {
+    try {
+        const fileStr = req.body.data;
+        const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+            upload_preset: 'dev_setups',
+        });
+        console.log(uploadResponse);
+        res.json({ msg: 'yaya' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ err: 'Something went wrong' });
+    }
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log('listening on 3001');
+});
---
+++ server/utils/cloudinary.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+module.exports = { cloudinary };
